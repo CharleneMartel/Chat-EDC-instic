b05c85635eef6d35e787dc2ebcf6689858d3c5af
CharleneMartel <89074088+CharleneMartel@users.noreply.github.com>
2021-08-25 09:53:53 +0200

    Update login.php

--- anonymeChat/script/login.php
+++ anonymeChat/script/login.php
@@ -1,29 +1,33 @@
 <?php
 session_start();
 require ('functions.php');
-$user_pseudo = 'anonymous'.mt_rand(0,2500);
-$is_user_connect = is_user_connect($user_pseudo);
+
+$is_user_connect = is_user_connect($_POST['pseudo']);
 if ($is_user_connect == false) {
 
 $bdd = bdd_connect();
     
-  
+$user_mdp = crypt_mdp($_POST['password']);
+  $user_pseudo = htmlspecialchars($_POST['pseudo']);
   
   $time = time();
-    
-  
-      user_connect($_SERVER['REMOTE_ADDR'], $user_pseudo);    
-      $_SESSION['pseudo'] = $user_pseudo;
-      
+    $reponse_mdp = $bdd->query('SELECT account_pass FROM chat_accounts WHERE account_login = ' . $bdd->quote($_POST['pseudo']));
+  $mdp = $reponse_mdp->fetchColumn();
+  if (isset($_POST['password']) && $user_mdp == $mdp) {
+      user_connect($_SERVER['REMOTE_ADDR'], $_POST['pseudo']);    
+      $_SESSION['pseudo'] = htmlspecialchars($_POST['pseudo']);
+      maj_connect();
       $_SESSION['ip'] = $_SERVER['REMOTE_ADDR'];
-      header('Location: ./../chat.php');
+      header('Location: cnx.php');
       
       }
       
-      
-        
+      else {
+        echo ('Mot de passe incorrect !');
+        }
+        }
             else {
-              echo 'Erreur : ce pseudo existe déjà !';
+              echo 'Erreur : vous êtez déjà connectés !';
       }
       
                 
