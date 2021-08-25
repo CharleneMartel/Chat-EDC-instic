67fc27c974580a8a4bf9eb97d2c0dd482c6f2f6c
CharleneMartel <89074088+CharleneMartel@users.noreply.github.com>
2021-08-25 09:53:45 +0200

    Update set_status.php

--- anonymeChat/script/set_status.php
+++ anonymeChat/script/set_status.php
@@ -7,7 +7,7 @@ if ($_SESSION['pseudo'] == NULL) {
     }
 else {
   $new_status = $_GET['status'];
-  $set_status = $bdd->prepare('UPDATE chat_onlineA SET online_status = :status WHERE online_user = :pseudo');
+  $set_status = $bdd->prepare('UPDATE chat_online SET online_status = :status WHERE online_user = :pseudo');
   $set_status->execute(array(
       'status' => $new_status,
       'pseudo' => $_SESSION['pseudo'],
